ed103bfa30bbcdb816bcfa60bf84b121af6db382
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-09-28 17:24:50 +0200

    added the service call in controller DI

--- src/main/java/edu/yacoubi/springbootmasterclass/customer/CustomerController.java
+++ src/main/java/edu/yacoubi/springbootmasterclass/customer/CustomerController.java
@@ -1,13 +1,21 @@
 package edu.yacoubi.springbootmasterclass.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
 
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
     @GetMapping
     public Customer getCustomer() {
-        return new Customer(1L, "Joe");
+        return customerService.getCustomer();
     }
 }
--- src/main/java/edu/yacoubi/springbootmasterclass/customer/CustomerService.java
+++ src/main/java/edu/yacoubi/springbootmasterclass/customer/CustomerService.java
@@ -1,4 +1,10 @@
 package edu.yacoubi.springbootmasterclass.customer;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class CustomerService {
+    public Customer getCustomer() {
+        return new Customer(1L, "Joe");
+    }
 }
