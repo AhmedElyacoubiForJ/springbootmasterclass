0c83fab86a8c6003468a65fbb9b5c61851cfd151
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-09-28 21:31:48 +0200

    specified the repo. primary implementation

--- src/main/java/edu/yacoubi/springbootmasterclass/customer/CustomerRepositoryImpl.java
+++ src/main/java/edu/yacoubi/springbootmasterclass/customer/CustomerRepositoryImpl.java
@@ -1,11 +1,13 @@
 package edu.yacoubi.springbootmasterclass.customer;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.List;
 
 @Component
+@Primary
 public class CustomerRepositoryImpl implements CustomerRepositoryInterface {
     @Override
     public List<Customer> getCustomers() {
