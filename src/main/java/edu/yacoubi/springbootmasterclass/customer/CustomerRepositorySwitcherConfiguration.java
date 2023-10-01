2e3fd8733cc71aeba854e3fb64c9cc89734e9ec7
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-10-01 15:48:24 +0200

    reading application properties through using Environment class injected in sonfig class

--- src/main/java/edu/yacoubi/springbootmasterclass/customer/CustomerRepositorySwitcherConfiguration.java
+++ src/main/java/edu/yacoubi/springbootmasterclass/customer/CustomerRepositorySwitcherConfiguration.java
@@ -1,9 +1,11 @@
 package edu.yacoubi.springbootmasterclass.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class CustomerRepositorySwitcherConfiguration {
@@ -17,11 +19,28 @@ public class CustomerRepositorySwitcherConfiguration {
     @Value("${info.company.name}")
     private String companyName;
 
+    @Autowired
+    private Environment environment;
+
     @Bean
     CommandLineRunner commandLineRunner() {
         return args -> {
             System.out.println("Command line runner hurry up and do configurations");
-            System.out.println("Company name : " + companyName);
+            System.out.println("Company name injected: " + companyName);
+            System.out.println();
+            System.out.println("Environment : ");
+            System.out.println("\t" + environment.getProperty("info.company.name"));
+            System.out.println("\t" + environment.getProperty("info.app.name"));
+            System.out.println("\t" + environment.getProperty("info.app.description"));
+            System.out.println("\t" + environment.getProperty("info.app.version"));
+
+
+
+
+
+
+
+            //System.out.println("Environment : " + environment);
         };
     }
 
